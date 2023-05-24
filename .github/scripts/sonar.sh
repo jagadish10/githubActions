756d3408bc41d1c9854482bde26debb1bb691930
Jagadish Gowda <jagadish.gowda@triconinfotech.com>
2023-05-24 16:24:42 +0530

    created new workflow for sonar

--- .github/scripts/sonar.sh
+++ .github/scripts/sonar.sh
@@ -4,7 +4,7 @@
 CURRENT_VERSION=$(curl -s -u "${SONAR_AUTH_TOKEN}:" "https://sonarcloud.io/api/project_analyses/search?project=${SONAR_PROJECT_KEY}&ps=1" | jq -r '.analyses[0].version')
 
 # increment the project version number
-NEW_VERSION=$((CURRENT_VERSION + 0.0.1))
+NEW_VERSION=$((CURRENT_VERSION + 1))
  
 # call the SonarCloud API to update the project version
 curl -u "${SONAR_AUTH_TOKEN}:" -X POST "https://sonarcloud.io/api/project_versions/set?project=${SONAR_PROJECT_KEY}&version=${NEW_VERSION}"
