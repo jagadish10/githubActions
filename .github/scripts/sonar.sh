8d1113169b4c786570d95161b55d510ffa159f93
Jagadish Gowda <jagadish.gowda@triconinfotech.com>
2023-05-24 17:41:50 +0530

    modified sonar script

--- .github/scripts/sonar.sh
+++ .github/scripts/sonar.sh
@@ -5,7 +5,9 @@ CURRENT_VERSION=$(curl -s -u "${SONAR_AUTH_TOKEN}:" "https://sonarcloud.io/api/p
 echo $CURRENT_VERSION
 
 # increment the project version number
-NEW_VERSION=$((CURRENT_VERSION + 1))
+INCREMENT_NUMBER=0.0.1
+# NEW_VERSION=$((CURRENT_VERSION + 0.0.1 | bc))
+NEW_VERSION=$(echo "$CURRENT_VERSION + $INCREMENT_NUMBER" | bc)
 echo $NEW_VERSION
 # call the SonarCloud API to update the project version
 curl -u "${SONAR_AUTH_TOKEN}:" -X POST "https://sonarcloud.io/api/project_versions/set?project=${SONAR_PROJECT_KEY}&version=${NEW_VERSION}"
